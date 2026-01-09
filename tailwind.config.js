3c59d1e73fdfa831a31d391d275bc6a3d760b857
Maurice van Dorst <maurice.vandorst@retail.nl>
2026-01-09 23:29:37 +0100

    Styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-// tailwind.config.js
 module.exports = {
   content: [
     "./*.html",
@@ -8,20 +7,8 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        // één zachte highlight die over je tekst schuift
-        'shimmer-text':
-          'linear-gradient(110deg, rgba(255,255,255,1) 0%, rgba(255, 255, 255, 0.75) 45%, rgba(255,255,255,1) 70%)',
-      },
-      keyframes: {
-        shimmer: {
-          '0%': { backgroundPosition: '-250% 0' },
-          '100%': { backgroundPosition: '250% 0' },
-          '0%': { backgroundPosition: '-250% 0' },
-        },
-      },
-      animation: {
-        shimmer: 'shimmer 15s ease-in-out infinite',
+      fontFamily: {
+        sans: ["Montserrat", "ui-sans-serif", "system-ui", "sans-serif"],
       },
     },
   },
